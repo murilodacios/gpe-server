39a542d7145df7ff67058211c173d40ac9e8ff1f
Murilo Santos <murilo.santos@itaborai.rj.gov.br>
2022-03-31 10:18:40 -0300

    Remove password seed env

--- prisma/seeds/admin.ts
+++ prisma/seeds/admin.ts
@@ -5,7 +5,7 @@ export const admin =
     name: "Murilo Dácio",
     email: "murilodacio@gmail.com",
     //@ts-ignore
-    password: hashSync(process.env.SEED_ADMIN, 8),
+    password: hashSync("", 8),
     level: 1,
     permissions: "god",
     job_id: "54933856-27cf-473a-ad75-7d20da7f589b"
