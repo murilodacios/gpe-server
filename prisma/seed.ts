9c0ff5786b9b42bc1fa766848acd5714937f2637
Murilo Santos <murilo.santos@itaborai.rj.gov.br>
2022-03-31 10:30:38 -0300

    Remove seed folder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,18 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-import { admin } from './seeds/admin'
+import { hash } from 'bcryptjs';
+import { prisma } from './../src/db/prismaClient'
 
 async function main() {
 
     await prisma.user.create({
-        data: admin,
+        data: {
+            name: "Murilo Dácio",
+            email: "murilodacio@gmail.com",
+            //@ts-ignore
+            password: await hash("", 8),
+            level: 1,
+            permissions: "god",
+            job_id: "54933856-27cf-473a-ad75-7d20da7f589b"
+        },
     });
 }
 
