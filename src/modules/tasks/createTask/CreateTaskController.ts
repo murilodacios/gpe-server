1e4c53bd0299f8770b4c226e269e53862b5bca49
Murilo Santos <murilo.santos@itaborai.rj.gov.br>
2022-04-18 16:54:57 -0300

    Fix: Import Create Task use case

--- src/modules/tasks/createTask/CreateTaskController.ts
+++ src/modules/tasks/createTask/CreateTaskController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CreateTaskUseCase } from "./createTaskUseCase";
+import { CreateTaskUseCase } from "./CreateTaskUseCase";
 
 class CreateTaskController {
 
